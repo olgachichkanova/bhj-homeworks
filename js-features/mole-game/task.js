18e9dcc33d85dc71187751b43289639bdbdccf3e
trideviataya <olgachichkanova@gmail.com>
2019-06-14 13:44:59 +0800

    mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,51 +1,35 @@
-//to do: count click as win if hole_has-mole
+//count click as win if hole_has-mole, count click as lost if no
 let winCounter = document.getElementById('dead');
 let lostCounter = document.getElementById('lost');
-let indexes = [];
+const holes = [];
 
 function getHole(index) {
     return document.getElementById(`hole${index}`);
 }
 
 for(let i = 1; i < 10; i++) {
-    indexes.push(i);
-}
-let holes = [];
-
-indexes.forEach(function(index) {
-    holes.push(getHole(index));
-});
-
-let active;
-
-function checkHole(hole) {
-    if (hole.className.includes( 'hole_has-mole' )) {
-        active = true;
-    } else {
-        active = false;
-    }
-    return active;
+    holes.push(getHole(i));
 }
 
 holes.forEach(function(hole) {
-    checkHole(hole);
+    hole.onclick = function() {
+        if (this.className.includes( 'hole_has-mole' )) {
+            winCounter.textContent++;
+            if (winCounter.textContent > 9)  {
+                alert('Победа!');
+                winCounter.textContent = 0;
+                lostCounter.textContent = 0;
+            }
+        } else {
+            lostCounter.textContent++;
+            if (lostCounter.textContent > 4) {
+                alert('Попробуйте еще раз!');
+                winCounter.textContent = 0;
+                lostCounter.textContent = 0;
+            }
+        }
+    }
 })
 
-if (active) {
-    winCounter.textContent++;
-} else {
-    lostCounter++;
-}
-
-//to do: count click as lost if hole is empty
-//to do: if has 10 moles alert 'You won!'
-// if (winCounter = 10) {
-//     alert('Победа!')
-// }
-//to do: if missed 5 times alert 'Oops! Try again!'
-// if (lostCounter = 5) {
-//     alert('Попробуйте еще раз!')
-// }
-//to do: clear the statistic after any alert
 
 
