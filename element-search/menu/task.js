640d8b00449b2b4b9bc358f59d8c84c42526830e
trideviataya <olgachichkanova@gmail.com>
2019-06-20 10:22:14 +0800

    corrections to the second homework

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,17 @@
 const links = document.querySelectorAll('a.menu__link');
 
+const menu = document.querySelectorAll('ul.menu_sub');
+
 function showMenu(elementName) {
     elementName.classList.add('menu_active');
 }
 
 links.forEach(function (link) {
     link.onclick = function() {
+//remove class menu_active to close all other sub menu on click
+        menu.forEach(function(item) {
+            item.classList.remove('menu_active');
+        });
         showMenu(this.nextElementSibling);
         return false;
     }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,20 +2,30 @@ const prev = document.querySelector('div.slider__arrow_prev');
 const next = document.querySelector('div.slider__arrow_next');
 const items = [...document.querySelectorAll('div.slider__item')];
 
-let sliderIndex = 0;
-
-prev.onclick = function() {
-    sliderIndex--;
-}
-
-next.onclick = function() {
-    sliderIndex++;
-}
+let i = 0;
 
 function active(elementName) {
     elementName.classList.add('slider__item_active');
 }
 
-for(let i = 0; i < items.length; i++) {
+prev.onclick = function() {
+    if(i == 0) {
+        i = items.length;
+    }
+    i--;
+    items.forEach(function(item) {
+        item.classList.remove('slider__item_active');
+    });
+    active(items[i]);
+}
+
+next.onclick = function() {
+    if (i == items.length - 1) {
+        i = -1;
+    }
+    i++;
+    items.forEach(function(item) {
+        item.classList.remove('slider__item_active');
+    });
     active(items[i]);
 }
