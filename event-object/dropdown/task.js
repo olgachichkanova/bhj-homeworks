ba7b0ec79df8847271e4d93c9e557639b2aad78c
trideviataya <olgachichkanova@gmail.com>
2019-06-25 11:39:36 +0800

    the third homework modified

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,13 +1,13 @@
 const dropdown = document.querySelector('ul.dropdown__list');
 const list = document.querySelector('div.dropdown__value');
-const dropdownItems = [...document.querySelectorAll('a.dropdown__link')];
+const dropdownItems = document.querySelectorAll('.dropdown__link');
 
 list.addEventListener('click', function() {
     dropdown.classList.toggle('dropdown__list_active')
     if (dropdown.classList.contains('dropdown__list_active')) {
         for (let i = 0; i < dropdownItems.length; i++) {
-            dropdownItems[i].addEventListener('click', function() {
-                console.log('test');
+            dropdownItems[i].addEventListener('click', function(item) {
+                item.preventDefault();
                 list.textContent = this.textContent;
                 return false;
             }, false);   
