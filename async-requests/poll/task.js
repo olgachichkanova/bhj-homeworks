192e9212962c924b7b7b98e36419ba57d57064fc
trideviataya <olgachichkanova@gmail.com>
2019-07-29 13:32:15 +0800

    no message

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -13,17 +13,14 @@ const createAnswers = ((answer) => {
 
 //create request
 let request = new XMLHttpRequest();
-request.addEventListener('load', onLoad);
+request.addEventListener('readystatechange', () => {
+    if (request.readyState === request.DONE) {
+        foo(JSON.parse(request.responseText));
+    }
+});
 request.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php', true);
 request.send();
 
-function onLoad() {
-    if (request.readyState === 4 && request.status === 200) {
-        foo(JSON.parse(request.responseText));
-    } else if (request.readyState !== 4 && request.status !== 200) {
-        console.log(`Ответ ${request.status}: ${request.statusText}`);
-    }
-}
 //download poll
 function foo(text) {
     console.log(text);
@@ -33,34 +30,34 @@ function foo(text) {
 
     pollTitle.innerText = title;
     
-    for(let item of answers) {
+    answers.forEach(item => {
+        pollAnswers.insertAdjacentHTML('afterbegin', createAnswers(item));
+    });
+    
+    const pollAnswer = [...pollAnswers.getElementsByClassName('poll__answer')];
 
-            pollAnswers.insertAdjacentHTML('afterbegin', createAnswers(item));
-        };
-    const pollAnswer = pollAnswers.getElementsByClassName('poll__answer');
-
-    for (let button of pollAnswer) {
+    pollAnswer.forEach(button => {
         button.addEventListener('click', e => {
             e.preventDefault();
             alert('Спасибо, ваш голос засчитан');
-        })
-    }
+        });
+    });
 
-     let requestPost = new XMLHttpRequest();
-            requestPost.addEventListener('load', () => {
-                if (requestPost.readyState === 4 && requestPost.status === 200) {
-                fooPost(JSON.parse(requestPost.responseText));
-                } else if (requestPost.readyState !== 4 && requestPost.status !== 200) {
-                console.log(`Ответ ${requestPost.status}: ${requestPost.statusText}`);
-                }
-            });
-            requestPost.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php', true);
-            requestPost.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-            requestPost.send('vote=data.id&answer=item');
+    //  let requestPost = new XMLHttpRequest();
+    //         requestPost.addEventListener('load', () => {
+    //             if (requestPost.readyState === 4 && requestPost.status === 200) {
+    //             fooPost(JSON.parse(requestPost.responseText));
+    //             } else if (requestPost.readyState !== 4 && requestPost.status !== 200) {
+    //             console.log(`Ответ ${requestPost.status}: ${requestPost.statusText}`);
+    //             }
+    //         });
+    //         requestPost.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php', true);
+    //         requestPost.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    //         requestPost.send('vote=data.id&answer=item');
 
 
-            function fooPost(dataPost) {
-                let answers = dataPost;
-                console.log(answers);
-            }
+    //         function fooPost(dataPost) {
+    //             let answers = dataPost;
+    //             console.log(answers);
+    //         }
 };
