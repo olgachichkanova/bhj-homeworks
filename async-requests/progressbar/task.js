cf70905329da0caceceab9205302bd34f694bdac
trideviataya <olgachichkanova@gmail.com>
2019-07-30 15:57:43 +0800

    no message

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,37 +1,44 @@
 const progressBar = document.getElementById('progress');
 const form = document.forms.form;
+let formData = new FormData(form);
 
-function upload(file) {
+
+function eventHandler(event) {
+    let loaded = event.loaded;
+    let total = event.total;
+    progressBar.setAttribute('value', loaded/total);
+    return false;
+};
+
+function addListeners (xhr, event) {
+    xhr.addEventListener('loadstart', eventHandler(event));
+    xhr.addEventListener('load', eventHandler(event));
+    xhr.addEventListener('loadend', eventHandler(event));
+    xhr.addEventListener('progress', eventHandler(event));
+    xhr.addEventListener('error', eventHandler(event));
+};
+
+function upload(formData) {
     let request = new XMLHttpRequest();
-
-    request.upload.onprogress = function(event) {
-        let loaded = event.loaded;
-        let total = event.total;
-
-        progressBar.setAttribute('value', loaded/total);
-        
-        return false;
-    };
-
     request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php', true);
-    request.send(file);
-
+    addListeners(request, event);
+    request.send(formData);
 }
 
 
 form.addEventListener('submit', (e) => {
 
     e.preventDefault();
+    
     let input = form.elements.file;
     let file = input.files[0];
-    
+    formData.append('file', file);
 
-    if (file) {
-        upload(file);
+    if (formData) {
+        upload(formData);
     };
-    
+
+    return false;
 });
-
-
 
 
