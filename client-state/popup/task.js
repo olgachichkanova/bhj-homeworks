9235d12a8ed48db6fadd4e4673859146c54445cc
trideviataya <olgachichkanova@gmail.com>
2019-08-02 15:41:36 +0800

    no message

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,30 +1,39 @@
 const popup = document.querySelector('.modal');
 const button = document.querySelector('.modal__close');
 
+document.cookie = "user=Olga;";
+
+console.log(document.cookie);
+
+
 const getCookie = (name) => {
-    const value = ';' + document.cookie;
+    const value = ";" + document.cookie;
     
-    let elements = value.split(';' + name + '=');
+    let elements = value.split(";" + name + "=");
+    
     if (elements.length === 2) {
         return elements
             .pop()
-            .split(';')
+            .split(";")
             .shift();
     }
 }
+// console.log(getCookie('user'));
+
+// console.log(document.cookie);
 
 const checkCookie = () => {
     let userName = getCookie('user');
     console.log(userName);
-    if (userName === '') {
+    if (userName == '') {
         popup.classList.add('modal_active');
         button.addEventListener('click', () => {
         popup.classList.remove('modal_active');
         });
-        document.cookie('user=username');
+        document.cookie = 'user=username';
     } 
 }
 
-document.onload = checkCookie();
+// document.onload = checkCookie();
 
 
--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,21 +1,14 @@
-const items = document.querySelectorAll('.interest__check');
-const lists = document.querySelectorAll('.interests_active');
-let main = [];
-let mainInterests = [];
-
-//trying to get input element
-lists.forEach((list) => {
-    main.push(list.parentElement);
-});
-
-main.forEach((item) => {
-    mainInterests.push(item.querySelector('input'));
-});
-
-// trying to set up the event
-mainInterests.forEach((i) => {
-    i.addEventListener('change', () => {
-        if (i.checked) {
+const items = [...document.querySelectorAll('.interest__check')];
+const lists = [...document.querySelectorAll('.interests_active')];
+const interestList = [...document.querySelectorAll('.interest')];
+console.log(items);
+console.log(lists);
+console.log(interestList);
+for(let i in interestList) {
+    // console.log(interestList[i]);
+    
+    interestList[i].addEventListener('change', () => {
+        if (interestList[i].checked) {
             items.forEach((item) => {
                 item.checked = true;
             });
@@ -25,4 +18,31 @@ mainInterests.forEach((i) => {
             });
         };
     });
-});
+}
+
+// let main = [];
+// let mainInterests = [];
+
+//trying to get input element
+// lists.forEach((list) => {
+//     main.push(list.parentElement);
+// });
+
+// main.forEach((item) => {
+//     mainInterests.push(item.querySelector('input'));
+// });
+
+// // trying to set up the event
+// mainInterests.forEach((i) => {
+//     i.addEventListener('change', () => {
+//         if (i.checked) {
+//             items.forEach((item) => {
+//                 item.checked = true;
+//             });
+//         } else {
+//             items.forEach((item) => {
+//                 item.checked = false;
+//             });
+//         };
+//     });
+// });
