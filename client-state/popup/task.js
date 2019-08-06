28deab250c9d9497d5a1383929457294a3eeeb7c
trideviataya <olgachichkanova@gmail.com>
2019-08-06 09:04:24 +0800

    no message

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,39 +1,17 @@
 const popup = document.querySelector('.modal');
 const button = document.querySelector('.modal__close');
 
-document.cookie = "user=Olga;";
-
-console.log(document.cookie);
-
-
-const getCookie = (name) => {
-    const value = ";" + document.cookie;
-    
-    let elements = value.split(";" + name + "=");
-    
-    if (elements.length === 2) {
-        return elements
-            .pop()
-            .split(";")
-            .shift();
-    }
-}
-// console.log(getCookie('user'));
-
-// console.log(document.cookie);
+button.addEventListener('click', () => {
+    popup.classList.remove('modal_active');
+    document.cookie = 'modal=true';
+});
 
 const checkCookie = () => {
-    let userName = getCookie('user');
-    console.log(userName);
-    if (userName == '') {
+    if (document.cookie.indexOf() < 0) {
         popup.classList.add('modal_active');
-        button.addEventListener('click', () => {
-        popup.classList.remove('modal_active');
-        });
-        document.cookie = 'user=username';
-    } 
-}
+    } ;
+};
 
-// document.onload = checkCookie();
+document.onload = checkCookie();
 
 
