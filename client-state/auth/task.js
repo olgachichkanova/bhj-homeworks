aa2f5a28e5ef29c6360007607dc4e7fd07e64657
trideviataya <olgachichkanova@gmail.com>
2019-08-20 11:09:32 +0800

    no message

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -2,30 +2,27 @@ const login = document.getElementById('signin');
 const loginForm = document.getElementById('signin__form');
 const welcome = document.getElementById('welcome');
 const user = document.getElementById('user_id');
-// const button = document.getElementById('signin__btn');
 
 login.classList.add('signin_active');
-
-
 
 let storageData = localStorage.getItem('id');
 
 function checkLocalStorage () {
-    if(storageData.length > 0) {
+    if(storageData !== null) {
         welcome.classList.add('welcome_active');
         login.classList.remove('signin_active');
         user.innerText = localStorage.getItem('id');
     } else {
         loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+            e.preventDefault();
+            
+            let formData = new FormData(loginForm);
 
-    let formData = new FormData(loginForm);
-
-    let request = new XMLHttpRequest();
-    request.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-    request.addEventListener('readystatechange', () => {
-        if(request.readyState == request.DONE && request.status == 200) {
-            foo(JSON.parse(request.responseText));
+            let request = new XMLHttpRequest();
+            request.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+            request.addEventListener('readystatechange', () => {
+                if(request.readyState == request.DONE && request.status == 200) {
+                    foo(JSON.parse(request.responseText));
         }
     })
     request.send(formData);
@@ -47,4 +44,5 @@ function foo (data) {
     
 };
 
-document.onload - checkLocalStorage();
+document.onload = checkLocalStorage();
+
